5a114650e4b8a924ca62328c50f6657d7bd2ce14
jmdvirus <ciyingjue@163.com>
2015-10-19 15:52:12 +0800

    check head valid
    the head will be check each command.

--- goproj/src/jk/jkprotocol/protocol.go
+++ goproj/src/jk/jkprotocol/protocol.go
@@ -2,9 +2,12 @@ package jkprotocol
 
 import (
 	// "jk/jkcommon"
+	"fmt"
 	"jk/jklog"
+	"jk/kfmd5"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -22,11 +25,17 @@ const (
 	JK_PROTOCOL_CMD_SAVEFILE = 1 << iota
 )
 
+const (
+	jk_protocol_key = "ab3w-be82f231-aa-cd1b"
+)
+
 type JKProtocolHead struct {
 	version    string
 	command    int
 	subcommand int
 	id         string
+	timeval    int64
+	sign       string
 }
 
 type JKProtocol struct {
@@ -42,8 +51,12 @@ func NewJKProtocol() *JKProtocol {
 }
 
 func (p *JKProtocol) toString() string {
-	return p.head.version + jk_seperate + p.head.id + jk_seperate +
-		strconv.Itoa(p.head.command) + jk_seperate + strconv.Itoa(p.head.subcommand)
+	p.head.timeval = time.Now().Unix()
+	firststring := p.head.version + jk_seperate + p.head.id + jk_seperate +
+		strconv.Itoa(p.head.command) + jk_seperate + strconv.Itoa(p.head.subcommand) + jk_seperate +
+		fmt.Sprintf("%d", p.head.timeval)
+	key := kfmd5.Sum([]byte(firststring))
+	return firststring + jk_seperate + fmt.Sprintf("%x", key)
 }
 
 func (p *JKProtocol) GenerateRegister(id string) string {
@@ -74,17 +87,28 @@ func (p *JKProtocol) GenerateResponseFail() string {
 	return p.toString() + jk_seperate + p.data
 }
 
+func (p *JKProtocol) checkJKProtocolHeadValid() bool {
+	firststring := p.head.version + jk_seperate + p.head.id + jk_seperate +
+		strconv.Itoa(p.head.command) + jk_seperate + strconv.Itoa(p.head.subcommand) + jk_seperate +
+		fmt.Sprintf("%d", p.head.timeval)
+	key := kfmd5.Sum([]byte(firststring))
+	if strings.Compare(p.head.sign, fmt.Sprintf("%x", key)) == 0 {
+		return true
+	}
+	return false
+}
+
 // Server parse files
 func ParseJKProtocol(data string) *JKProtocol {
 	// jklog.L().Debugln("data: ", data)
 	strs := strings.Split(data, jk_seperate)
 
-	if len(strs) < 4 {
+	if len(strs) < 6 {
 		jklog.L().Errorln("wrong length")
 		return nil
 	}
 	if strings.Compare(strs[0], jk_version) != 0 {
-		jklog.L().Errorln("wrong version")
+		jklog.Lfile().Errorln("wrong version")
 		return nil
 	}
 	cmd, err := strconv.Atoi(strs[2])
@@ -101,9 +125,21 @@ func ParseJKProtocol(data string) *JKProtocol {
 	p.head.subcommand = subcmd
 	p.head.version = strs[0]
 	p.head.id = strs[1]
+	// p.head.timeval = strconv.Atoi(strs[4])
+	p.head.timeval, err = strconv.ParseInt(strs[4], 10, 64)
+	if err != nil {
+		jklog.Lfile().Errorln("parse time value failed. ", err)
+		return nil
+	}
+	p.head.sign = strs[5]
+
+	if !p.checkJKProtocolHeadValid() {
+		jklog.Lfile().Errorln("head valid check fail")
+		return nil
+	}
 
 	headlen := len(p.head.version) + len(p.head.id) + len(strs[2]) +
-		len(strs[3]) + len(jk_seperate)*4
+		len(strs[3]) + len(jk_seperate)*6 + len(strs[4]) + len(p.head.sign)
 	// datalen := len(data) - headlen
 
 	if headlen < len(data) {
@@ -129,7 +165,7 @@ func (p *JKProtocol) ParseFilename() string {
 	if p.head.command == JK_PROTOCOL_CMD_NOTIFY && p.head.subcommand == JK_PROTOCOL_CMD_SAVEFILE {
 		strs := strings.Split(p.data, jk_seperate)
 		if len(strs) < 1 {
-			jklog.L().Errorln("length error")
+			jklog.Lfile().Errorln("length error")
 			return ""
 		}
 		return strs[0]
@@ -141,7 +177,7 @@ func (p *JKProtocol) ParseFilenameData() (string, string) {
 	if p.head.command == JK_PROTOCOL_CMD_CONTROL && p.head.subcommand == JK_PROTOCOL_CMD_SAVEFILE {
 		strs := strings.Split(p.data, jk_seperate)
 		if len(strs) < 2 {
-			jklog.L().Errorln("length error")
+			jklog.Lfile().Errorln("length error")
 			return "", ""
 		}
 		return strs[0], strs[1]
