0f84cc588fb7650a5eb97231981f91e07c99f891
jmdvirus <ciyingjue@163.com>
2016-08-26 01:29:13 +0800

    fix: jkmqtt

---
+++ src/jkprog/jkmqttcli/config.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type mqtt_param struct {
+	Broker    string   `json:broker`
+	User      string   `json:user`
+	Password  string   `json:password`
+	Id        string   `json:id`
+	Cleansess bool     `json:cleansess`
+	Qos       int      `json:qos`
+}
+
+func init_config(path string) *mqtt_param {
+	mp := &mqtt_param{}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil
+	}
+	err = json.Unmarshal(data, mp)
+	if err != nil {
+		return nil
+	}
+	return mp
+}
---
+++ src/jkprog/jkmqttcli/jkmqttcli.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"jk/jklog"
+	"time"
+)
+
+/*
+{
+  "broker":"tcp://localhost:1883",
+  "user":"",
+  "password":"",
+  "
+}
+ */
+var (
+	conffile = flag.String("conf", "./config.json", "Configure file")
+)
+
+func main() {
+	flag.Parse()
+
+	conf := init_config(*conffile)
+	if conf == nil {
+		jklog.L().Errorln("Configure read failed, please check.")
+		return
+	}
+
+	jklog.L().Debugln(conf)
+
+	mq, err := InitOption(conf)
+	if err != nil {
+		jklog.L().Errorln("init option failed ", err)
+		return
+	}
+
+	jklog.L().Debugln("Start to init subscribe")
+	// connect for subscribe
+	sub_api := &MqttOptionSub{}
+	err = sub_api.Init(mq.Opt, conf, 5, "api")
+	if err != nil {
+		jklog.L().Errorln("failed subscript ", err)
+	}
+
+	// if we want subscribe exit
+	// sub_api.Exit = <-true
+
+	//
+	// Publish
+	//
+	/*
+	jklog.L().Debugln("Start to init publish")
+	// When we need publish
+	err = mq.Init()
+	if err != nil {
+		jklog.L().Errorln("publish failed ", err)
+	}
+	*/
+
+	/*
+	for {
+		time.Sleep(time.Millisecond * 4000)
+		jklog.L().Debugln("Send publish")
+		// we can send data now if ok
+		publish := [2]string{
+			"api", "this is test",
+		}
+		mq.Pub.Data <- publish
+	}
+	*/
+
+	for {
+		time.Sleep(time.Millisecond*500)
+	}
+}
