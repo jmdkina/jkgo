50d4787f9d67a6966e843370f6227f438577f8e4
jmdkina <ciyingjue@163.com>
2017-04-01 08:37:31 +0800

    update:   program for one file

--- src/jkprog/jkitoy/itoy.go
+++ src/jkprog/jkitoy/itoy.go
@@ -2,19 +2,59 @@ package main
 
 import (
 	"flag"
-	"io"
+	//"io"
 	"jk/jklog"
 	. "jk/jkitoy"
 )
 
 var (
-	filename     = flag.String("filename", "", "what file to do")
-	saveposition = flag.String("saveposition", "./", "Where to save")
+	filename     = flag.String("f", "", "filename to do")
+	saveposition = flag.String("s", "", "Where to save")
+	savefilename = flag.String("n", "", "Filename when save")
 	revert       = flag.Bool("revert", false, "What style you use")
 )
 
+func itoy_do_base() {
+	itoy, err := NewItoYItem(*filename)
+	if err != nil {
+		jklog.L().Errorf("Create error %v\n", err)
+		return
+	}
+	itoy.SetSavePosition(*saveposition, *savefilename)
+	err = itoy.DoSomething()
+	if err != nil {
+		jklog.L().Errorf("do something %v\n", err)
+	}
+}
+
+func itoy_do_travers() {
+	itoy, err := NewItoYItem(*filename)
+	if err != nil {
+		jklog.L().Errorf("travers create %v\n", err)
+		return
+	}
+	itoy.SetTraversPosition(*saveposition, *savefilename)
+	err = itoy.Travers()
+	if err != nil {
+		jklog.L().Errorf("travers do %v\n", err)
+	}
+}
+
 func main() {
 	flag.Parse()
+	if len(*filename) == 0 || len(*saveposition) == 0 || len(*savefilename) == 0 {
+		jklog.L().Errorln("Arges need")
+		return
+	}
+
+	if *revert {
+		itoy_do_travers()
+	} else {
+		itoy_do_base()
+	}
+	return
+
+	/*
 	if len(*filename) == 0 {
 		jklog.L().Errorln("I must know what file to do with ...")
 		return
@@ -40,4 +80,5 @@ func main() {
 
 	jklog.L().Infoln("Do it success ...")
 	return
+	*/
 }
