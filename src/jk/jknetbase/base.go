7ea3a181eb27e4682df5338f59ef92cf4c1a218c
jmdkina <ciyingjue@163.com>
2017-06-06 16:44:45 +0800

    update:   add jknetbase

---
+++ src/jk/jknetbase/base.go
@@ -0,0 +1,119 @@
+package jknetbase
+
+import (
+	"net"
+	"errors"
+	"strconv"
+	"io"
+)
+
+type JKNetBaseRecv struct {
+	listener   net.Listener
+
+	address    string
+	port       int
+	nettype    int
+
+	items      map[string](JKNetBaseRecvItem)
+}
+
+type JKNetBaseRecvItem struct {
+	conn        net.Conn
+	remoteaddr  string
+	data        []string
+}
+
+func (nb *JKNetBaseRecv) New(addr string, port int, nettype int) error {
+	nb.address = addr
+	nb.port = port
+	nb.nettype = nettype
+
+	if nettype == 0 {
+		var err error
+		nb.listener, err = net.Listen("tcp", addr + ":" + strconv.Itoa(port))
+		if err != nil {
+			return err
+		}
+	} else {
+		return errors.New("Unsupported net")
+	}
+	return nil
+}
+
+func (nb *JKNetBaseRecv) Recv() error {
+	for {
+		conn, err := nb.listener.Accept()
+		if err != nil {
+			return err
+		}
+		go func() {
+			for {
+				rdata := make([]byte, 2 << 15)
+				n, err := conn.Read(rdata)
+				if err == io.EOF {
+					break
+				}
+
+				item := JKNetBaseRecvItem{}
+				item.conn = conn
+				item.remoteaddr = conn.RemoteAddr().String()
+				item.data = append(item.data, string(rdata[0:n]))
+				nb.items[item.remoteaddr] = item
+				nb.handle_msg(conn, item.data)
+			}
+		}()
+	}
+	return nil
+}
+
+func (nb *JKNetBaseRecv) handle_msg(conn net.Conn, data []string) error {
+
+	return nil
+}
+
+type JKNetBaseClient struct {
+	address    string
+	port       int
+	nettype    int
+
+	conn net.Conn
+	recvdata string
+}
+
+func (nb *JKNetBaseClient) New(addr string, port int, nettype int) error {
+	if nettype == 1 {
+		nb.address = addr
+		nb.port = port
+		nb.nettype = nettype
+		conn, err := net.Dial("tcp", addr + ":" + strconv.Itoa(port))
+		if err != nil {
+			return err
+		}
+		nb.conn = conn
+	} else {
+		return errors.New("Unsupported nettype")
+	}
+	return nil
+}
+
+func (nb *JKNetBaseClient) Recv(data string) error {
+	rdata := make([]byte, 2<<15)
+	n, err := nb.conn.Read(rdata)
+	if err != nil {
+		return err
+	}
+	data = string(rdata[0:n])
+	nb.recvdata = data
+	return nil
+}
+
+func (nb *JKNetBaseClient) Send(data string) error {
+	n, err := nb.conn.Write([]byte(data))
+	if err != nil {
+		return err
+	}
+	if n != len(data) {
+		return errors.New("Send length != data length")
+	}
+	return nil
+}
