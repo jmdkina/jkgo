531a6c0e49f5dde99fe8063ecb215f1b2ab683d4
jmdvirus <ciyingjue@163.com>
2017-07-13 18:10:45 +0800

    update:    simpleserver: pacakge diffrent platform

--- package.sh
+++ package.sh
@@ -9,30 +9,38 @@ function log() {
 }
 
 VERSION=`date +%Y%m%d%H%M%S`
-PKG_NAME=$type-$VERSION.tar.bz2
+PKG_NAME=$type-$cross-$VERSION.tar.bz2
 
 case $cross in
     amd64)
     OS=linux
     ARCH=amd64
     bin_path=$basepath/bin/simpleserver
+    CROSS_ENV="GOOS=linux GOARCH=amd64"
     ;;
     corei7|386)
     OS=linux
     ARCH=386
-    bin_path=$basepath/linux_386/bin/simpleserver
+    bin_path=$basepath/bin/simpleserver
+    CROSS_ENV="GOOS=linux GOARCH=386"
     ;;
     windows)
     OS=windows
     ARCH=amd64
     bin_path=$basepath/bin/simpleserver
+    CROSS_ENV="GOOS=linux GOARCH=amd64"
     ;;
 esac
 
 case $type in
     simpleserver)
+    rm -rf $bin_path
     if [ ! -f $bin_path ]; then
-        ${CROSS_ENV} go install simpleserver
+        log "build GOOS=$OS GOARCH=$ARCH"
+        cd $basepath/src/jkprog/simpleserver
+        GOOS=$OS GOARCH=$ARCH go build simpleserver.go
+        cd -
+        mv $basepath/src/jkprog/simpleserver/simpleserver $bin_path
     fi
     if [ ! -f $bin_path ]; then
         log "Error: simpleserver not build, check it"
