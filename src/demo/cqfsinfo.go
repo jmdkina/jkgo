733e58e9805ebd51bb06c929a534384a3d9172e0
jmdkina <ciyingjue@163.com>
2017-07-21 14:20:51 +0800

    update:   CQFS:

---
+++ src/demo/cqfsinfo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"jk/jklog"
+)
+
+type CQFSHeader struct {
+	Magic       string
+	Version     uint32
+	Target      string
+}
+
+type CQFSInfo struct {
+	Header   CQFSHeader
+}
+
+func NewCQFS(path string) (*CQFSInfo, error) {
+	ci := &CQFSInfo{}
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	value := make([]byte, 4096)
+	_, err = f.Read(value)
+	if err != nil {
+		return nil, err
+	}
+	ci.Header.Magic = string(value[:3])
+	ii, _ := strconv.Atoi(string(value[3:4]))
+	ci.Header.Version = uint32(ii)
+	ci.Header.Target = string(value[5:133])
+	return ci, nil
+}
+
+func (ci *CQFSInfo) PrintHeader() {
+	jklog.L().Printf("Header magic [%s] version [%d], Target [%s]\n",
+		ci.Header.Magic, ci.Header.Version, ci.Header.Target)
+}
+
+func main() {
+	ci, err := NewCQFS("/opt/data/more/arges")
+	if err != nil {
+		jklog.L().Errorln(err)
+		return
+	}
+	ci.PrintHeader()
+}
