961f6a774cb2f2708043223e1d72c43f69062521
jmdkina <ciyingjue@163.com>
2017-09-02 10:16:39 +0800

    update:   struct

--- src/simpleserver/db_mongo.go
+++ src/simpleserver/db_mongo.go
@@ -16,47 +16,39 @@ type DBMongo struct {
 func NewDBMongo(path string) *DBMongo {
 	i := &DBMongo{}
 	i.SetPath(path)
-	http.HandleFunc("/db", i.ServeHttp)
+	i.SetFunc("/db", i)
 	return i
 }
 
-func (b *DBMongo) ServeHttp(w http.ResponseWriter, r *http.Request) {
-	/*
-	if r.RemoteAddr != "localhost" || r.RemoteAddr != "127.0.0.1" || r.RemoteAddr != "0.0.0.0" {
-		jklog.L().Warnln("not from localhost ignore")
-		http.Redirect(w, r, "/notexist", http.StatusFound)
-		return;
+func (b *DBMongo) Get(w http.ResponseWriter, r *http.Request) {
+	sp := SimpleParse{}
+	filename := b.path + "/db/db.html"
+	jklog.L().Debugf("Get html [%s]\n", filename)
+
+	err := sp.Parse(w, filename, "")
+	if err != nil {
+		jklog.L().Errorln("Parse error ", err)
 	}
-	*/
+}
 
-	if r.Method == "GET" {
-		sp := SimpleParse{}
-		filename := b.path + "/db/db.html"
-		jklog.L().Debugf("Get html [%s]\n", filename)
-
-		err := sp.Parse(w, filename, "")
-		if err != nil {
-			jklog.L().Errorln("Parse error ", err)
+func (b *DBMongo) Post(w http.ResponseWriter, r *http.Request) {
+	cmd := r.FormValue("jk")
+	res := utils.M{
+		"Status":200,
+	}
+	jklog.L().Debugln("deal with command ", cmd)
+	switch cmd {
+	case "query_dbs":
+		host := r.FormValue("host")
+		if b.m == nil {
+			b.m = helper.NewMongo("mongodb://" + host + "/")
 		}
-	} else if r.Method == "POST" {
-		cmd := r.FormValue("jk")
-		res := utils.M{
-			"Status":200,
-		}
-		jklog.L().Debugln("deal with command ", cmd)
-		switch cmd {
-		case "query_dbs":
-			host := r.FormValue("host")
-			if b.m == nil {
-				b.m = helper.NewMongo("mongodb://" + host + "/")
-			}
-			dbs, _ := b.m.DBSession().DatabaseNames()
-			res["Result"] = dbs
-			d, _ := json.Marshal(res)
-			w.Write(d)
-			break;
-		case "query_colls":
-			break;
-		}
+		dbs, _ := b.m.DBSession().DatabaseNames()
+		res["Result"] = dbs
+		d, _ := json.Marshal(res)
+		w.Write(d)
+		break;
+	case "query_colls":
+		break;
 	}
 }
--- src/simpleserver/start.go
+++ src/simpleserver/start.go
@@ -6,14 +6,49 @@ import (
 	"os"
 	"jk/jksys"
 	"io"
+	"reflect"
 )
 
 type Base struct {
 	path     string
+	child    interface{}
 }
 
 func (b *Base) SetPath(path string) {
 	b.path = path
+}
+
+func (b *Base) SetFunc(url string, child interface{}) {
+	b.child = child
+	http.HandleFunc(url, b.ServerHttp)
+}
+
+func (b *Base) ServerHttp(w http.ResponseWriter, r *http.Request) {
+	c := reflect.ValueOf(b.child)
+	inputs := make([]reflect.Value, 2)
+	inputs[0] = reflect.ValueOf(w)
+	inputs[1] = reflect.ValueOf(r)
+	jklog.L().Debugf("URL: %s\n", r.URL.String())
+
+    switch r.Method {
+	case "GET":
+		method := c.MethodByName("Get")
+		if method.IsValid() {
+			method.Call(inputs)
+		} else {
+			jklog.L().Warnln("Undefined GET")
+		}
+		break
+	case "POST":
+		method := c.MethodByName("Post")
+		if method.IsValid() {
+			method.Call(inputs)
+		} else {
+			jklog.L().Warnln("Undefined POST")
+		}
+		break
+
+	}
 }
 
 type NotFound struct {
