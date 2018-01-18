abfc4d5deddd4a16a650ab3da103b1cc794fb865
jmdvirus <ciyingjue@163.com>
2018-01-18 14:42:42 +0800

    update

--- html/js/shici/shici.js
+++ html/js/shici/shici.js
@@ -1,7 +1,8 @@
-log_enable(true)
+log_enable(false)
 
 var current_shici = 0;
 var query_length = 5;
+
 $(function(){
     // Vue.options.delimiters = ['{$', '#}'];
 
@@ -17,33 +18,47 @@ $(function(){
             shicis: []
         },
         beforeCreate: function() {
-            var str = "jk=shici&cmd=query_shicis" + "&index=" + current_shici +
-                "&length=" + query_length;
-            log_print(str);
-            $.ajax({
-                url: "shici",
-                method: "POST",
-                data: str,
-                success: function (response, textStatus) {
-                    // log_print("stockquery success" + response + ", result:" + textStatus);
-                    ret = $.parseJSON(response);
-                    try {
-                        ret = $.parseJSON(response);
-                        if (ret == undefined || ret.Status != 200) {
-                            layer.msg("Fail of " + ret.Status);
-                            return;
-                        }
-                        for (var i = 0; i < ret.Result.length; i++) {
-                            ret.Result[i]["createtimestr"] = time_unix2string(ret.Result[i]["createtime"]);
-                            ret.Result[i]["index"] = i;
-                            shici.shicis.push(ret.Result[i])
-                        }
-                        current_shici = shici.shicis.length;
-                    } catch(e) {
-                        layer.msg("Fail parse response " + response);
-                    }
-                }
-             });
+            load_shicis();
         },
+        methods: {
+            showmore: function() {
+                load_shicis();
+            }
+        }
     });
+
+
+function load_shicis() {
+    var str = "jk=shici&cmd=query_shicis&" +
+        "index=" + current_shici + "&length=" + query_length;
+    log_print(str);
+    $.ajax({
+        url: "shici",
+        method: "POST",
+        data: str,
+        success: function (response, textStatus) {
+            log_print("query new success" + response + ", result:" + textStatus);
+            try {
+                ret = $.parseJSON(response);
+                if (ret.Status != 200) {
+                    layer.msg("Fail of " + ret.Status);
+                    return;
+                }
+                if (ret.Result.length == 0) {
+                    $(".showmore").attr("disabled", true);
+                    $(".showmore").html("No more");
+                    return;
+                }
+                for (var i = 0; i < ret.Result.length; i++) {
+                    ret.Result[i]["createtimestr"] = time_unix2string(ret.Result[i]["createtime"]);
+                    ret.Result[i]["index"] = i;
+                    shici.shicis.push(ret.Result[i])
+                }
+                current_shici = shici.shicis.length;
+            } catch(e) {
+                layer.msg("Fail parse , err: " + e);
+            }
+        }
+    });
+}
 });
