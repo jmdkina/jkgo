2df7fb98c4873ec99c1f0c96171ec89c4a5e5752
jmdvirus <ciyingjue@163.com>
2018-02-11 15:48:15 +0800

    images

--- src/jkprog/jkcatch/images.go
+++ src/jkprog/jkcatch/images.go
@@ -13,12 +13,22 @@ import (
 	"net/http"
 )
 
-var (
-	url  = flag.String("url", "", "url to catch")
-	path = flag.String("path", ".", "where to save")
-)
+type CatchHuaban struct {
+	files_list []string
+	need_lists []string
+	url        string
+	path       string // where to save
+}
 
-func http_catch(url string) ([]byte, error) {
+func NewCatchHuaban(path string) (*CatchHuaban, error) {
+	ch := &CatchHuaban{
+		url:  "http://huaban.com/favorite/beauty/",
+		path: path,
+	}
+	return ch, nil
+}
+
+func (ch *CatchHuaban) http_catch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -27,51 +37,47 @@ func http_catch(url string) ([]byte, error) {
 	return buf, err
 }
 
-var files_list []string
-
-func find_data(data string) []string {
+func (ch *CatchHuaban) find_data(data string) []string {
 	i := strings.Index(data, "key")
 	if i > 0 {
 		tm := strings.Split(data[i:80+i], ":")
 		if len(tm) > 1 {
-			files_list = append(files_list, tm[1])
+			ch.files_list = append(ch.files_list, tm[1])
 			//fmt.Println(tm[1])
 		}
-		find_data(data[i+10:])
+		ch.find_data(data[i+10:])
 	}
 	return nil
 }
 
-var need_lists []string
-
-func parse_files_list() []string {
-	for _, v := range files_list {
+func (ch *CatchHuaban) parse_files_list() []string {
+	for _, v := range ch.files_list {
 		tm := strings.Split(v, "\"")
 		if len(tm) > 1 {
 			//fmt.Println(tm[1])
-			need_lists = append(need_lists, tm[1])
+			ch.need_lists = append(ch.need_lists, tm[1])
 		}
 	}
-	return need_lists
+	return ch.need_lists
 }
 
-func catch_data(url string) {
+func (ch *CatchHuaban) catch_data(url string) {
 	log4go.Debug("catch url [%s]", url)
-	resp, err := http_catch(url)
+	resp, err := ch.http_catch(url)
 	log4go.Debug("soup get done len [%d]", len(resp))
 	if err != nil {
 		log4go.Error("error: ", err)
 		return
 	}
-	find_data(string(resp))
-	parse_files_list()
+	ch.find_data(string(resp))
+	ch.parse_files_list()
 }
 
-func save_data(path string) {
-	for _, v := range need_lists {
+func (ch *CatchHuaban) save_data(path string) {
+	for _, v := range ch.need_lists {
 		fullpath := path + "/" + v + ".jpg"
 		fullurl := "http://img.hb.aicdn.com/" + v
-		buf, err := http_catch(fullurl)
+		buf, err := ch.http_catch(fullurl)
 		if err != nil {
 			fmt.Println("error ", err)
 			continue
@@ -81,10 +87,25 @@ func save_data(path string) {
 	}
 }
 
+func catch_huaban() {
+	ch, _ := NewCatchHuaban(*path)
+	url := "http://huaban.com/favorite/beauty/"
+	ch.catch_data(url)
+	ch.save_data(*path)
+}
+
+var (
+	option = flag.String("option", "", "which mode to run")
+	path   = flag.String("path", ".", "where to save")
+)
+
 func main() {
 	flag.Parse()
 
-	log4go.Debug("url : [%s]", *url)
-	catch_data(*url)
-	save_data(*path)
+	log4go.Debug("option: ", *option)
+	switch *option {
+	case "huaban":
+		catch_huaban()
+	}
+	log4go.Debug("Everythin done.")
 }
