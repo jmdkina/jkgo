d85521d823417536e50a66e63af7f70cbc723097
jmdkina <ciyingjue@163.com>
2018-12-03 16:52:27 +0800

    update:   add debug program

---
+++ src/sctek/scdebug/scdebug.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"jk/jklog"
+	"net"
+	"os"
+	"strconv"
+)
+
+var (
+	addr = flag.String("addr", "", "dial to")
+	port = flag.Int("port", 12310, "port to dial")
+)
+
+type SCDebug struct {
+	addr  string
+	port  int
+	nethd *net.UDPConn
+}
+
+func (sc *SCDebug) Connect(addr string, port int) error {
+	sc.addr = addr
+	sc.port = port
+	str := addr + ":" + strconv.Itoa(port)
+	addrto, err := net.ResolveUDPAddr("udp4", str)
+	if err != nil {
+		jklog.L().Errorln(err)
+		return err
+	}
+
+	sc.nethd, err = net.DialUDP("udp", nil, addrto)
+	if err != nil {
+		jklog.L().Errorln("dial udp failed ", err)
+		return err
+	}
+
+	jklog.L().Infof("connect to %s success\n", str)
+	return nil
+}
+
+func (sc *SCDebug) Close() {
+	sc.nethd.Close()
+}
+
+func (sc *SCDebug) Debug() {
+	rdata := make([]byte, 10240)
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Input > ")
+
+		data, _, err := reader.ReadLine()
+		if err != nil {
+			jklog.L().Errorln("read err ", err)
+			break
+		}
+		if string(data) == "quit" {
+			break
+		}
+		sc.nethd.Write([]byte(data))
+
+		n, err := sc.nethd.Read(rdata)
+		if err != nil {
+			jklog.L().Errorln("read err ", err)
+			break
+		}
+		jklog.L().Infof("Read out response [%d] [%s]\n", n, string(rdata[:n]))
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	sc := SCDebug{}
+
+	err := sc.Connect(*addr, *port)
+	if err != nil {
+		return
+	}
+
+	sc.Debug()
+	sc.Close()
+}
